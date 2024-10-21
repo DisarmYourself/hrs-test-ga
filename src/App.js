435de21d30fa2adcf2f20220ee81319a3c745e58
Sergiy Gryshcshuk <sergiy.gryshcshuk@pettersonapps.com>
2024-10-21 13:31:33 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{value}</p>
+      <button onClick={() => setValue(value + 1)}>Inc</button>
+      <button onClick={() => setValue(value - 1)}>Desc</button>
     </div>
   );
 }
